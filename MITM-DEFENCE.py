2b1ebbd4c2dedd521cd7ac60b29e81727173267d
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2024-04-23 22:06:56 +0300

    inital commit

---
+++ MITM-DEFENCE.py
@@ -0,0 +1,32 @@
+import os
+import time
+import datetime
+import http.client
+
+conn = http.client.HTTPConnection("45.84.188.101")
+
+print("* Açıldı.")
+
+def print_arp_table():
+    current_time = datetime.datetime.now().time()
+    arp_table = os.popen("arp -a").read()
+    mac_count = {}
+    for line in arp_table.split("\n"):
+        if "dynamic" in line:
+            ip = line.split()[0]
+            mac = line.split()[1]
+            if mac in mac_count:
+                mac_count[mac] += 1
+                if mac_count[mac] > 1:
+                    print("Sistem Saldırı altında !")
+                    print(" IP Adresi " + ip)
+                    print(" Zaman ", current_time)
+                    return
+            else:
+                mac_count[mac] = 1
+    print("    - Sistem Güvende")
+
+while True:
+    print_arp_table()
+    time.sleep(1)
+    
